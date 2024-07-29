56ca49ffc49700f75a5b22726df86beabce8ff51
Bojan <babic.bojan62@gmail.com>
2024-07-29 15:21:43 +0200

    Navbar responsive done

--- src/components/NavbarLinks.vue
+++ src/components/NavbarLinks.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full flex justify-between items-center">
-    <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+  <div class="w-full flex flex-col md:flex-row justify-between items-center">
+    <ul class="hidden md:flex md:px-4 md:mx-auto md:font-semibold md:font-heading md:space-x-4 lg:space-x-6 xl:space-x-12">
       <li><a href="#">Gallery</a></li>
       <li><a href="#">Albums</a></li>
       <li><a href="#">Radio</a></li>
@@ -8,20 +8,20 @@
       <li><a href="#">About Us</a></li>
     </ul>
 
-    <div class="hidden md:flex items-center space-x-5">
-      <a href="#" class="svg-icon">
+    <div class="hidden md:flex items-center space-x-3 lg:space-x-5">
+      <a href="https://music.youtube.com/search?q=pirati+lora+holliday" class="svg-icon">
         <img src="@/assets/svg/youtube.svg" alt="YouTube" class="w-6 h-6">
       </a>
-      <a href="#" class="svg-icon">
+      <a href="https://open.spotify.com/album/7q0dHs3EZYLrydAKRktZ25?si=mRHxBb3US5a3dZRhzQZktg" class="svg-icon">
         <img src="@/assets/svg/spotify.svg" alt="Spotify" class="w-6 h-6">
       </a>
-      <a href="#" class="svg-icon">
+      <a href="https://music.apple.com/us/album/lora-holliday/1748322338" class="svg-icon">
         <img src="@/assets/svg/applemusic.svg" alt="Apple Music" class="w-6 h-6">
       </a>
-      <a href="#" class="svg-icon">
+      <a href="https://www.deezer.com/search/pirati%20lora%20holliday" class="svg-icon">
         <img src="@/assets/svg/deezer.svg" alt="Deezer" class="w-6 h-6">
       </a>
-      <a href="#" class="svg-icon">
+      <a href="https://www.discogs.com/release/5517537-Pirati-Lora-Holliday" class="svg-icon">
         <img src="@/assets/svg/discogs.svg" alt="Discogs" class="w-6 h-6">
       </a>
     </div>
@@ -60,6 +60,8 @@ a:hover {
 
 .svg-icon:hover img {
   transform: scale(1.1);
-  filter: invert(1) brightness(2);
+  filter: invert(1) contrast(1.2);
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
 }
 </style>
