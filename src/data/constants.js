1c7191472ec1b63a7cc4ef7351c86b42226f56ef
Andy <shikesenpu@gmail.com>
2024-08-02 10:48:38 +0200

    Static images

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,9 +1,17 @@
+// export const carouselImages = [
+//     "https://i.imgur.com/7o6gXKY.png",
+//     "https://i.imgur.com/g7HiNOC.png",
+//     "https://i.imgur.com/6pzkvIp.png",
+//     "https://i.imgur.com/7DLTIRr.png",
+//     "https://i.imgur.com/fqFVfaj.png",
+//     "https://i.imgur.com/ppCQohw.png",
+//   ];
+
 export const carouselImages = [
-    "https://i.imgur.com/7o6gXKY.png",
-    "https://i.imgur.com/g7HiNOC.png",
-    "https://i.imgur.com/6pzkvIp.png",
-    "https://i.imgur.com/7DLTIRr.png",
-    "https://i.imgur.com/fqFVfaj.png",
-    "https://i.imgur.com/ppCQohw.png",
-  ];
-  
+  "src/assets/CarouselImages/1.png",
+  "src/assets/CarouselImages/2.png",
+  "src/assets/CarouselImages/3.png",
+  "src/assets/CarouselImages/4.png",
+  "src/assets/CarouselImages/5.png",
+  "src/assets/CarouselImages/6.png",
+];
