24fed4c1d53e1d655d33d2c117d7607864b449fb
Bojan <babic.bojan62@gmail.com>
2024-08-08 03:49:15 +0200

    Hero animation

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,3 +1,31 @@
 <template>
   <div class="background"></div>
 </template>
+
+<script>
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+
+export default {
+  name: 'Background',
+  setup() {
+    onMounted(() => {
+      gsap.to('.background', {
+        backgroundPosition: '200% 200%',
+        duration: 20,
+        repeat: -1,
+        ease: 'linear'
+      });
+    });
+  }
+};
+</script>
+
+<style scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  background-size: 400% 400%;
+  background: url(../assets/image.jpg);
+}
+</style>
